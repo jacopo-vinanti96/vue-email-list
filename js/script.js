fd9066a2173d9b3a47aed64d2769c0b25372dccf
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-04-07 16:19:41 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 const app = new Vue ({
   el: '#email-generator',
   data: {
-    emails: [
-      {
-        key: 'ciao'
-      }
-    ],
-    emailQuantity: 1
+    emailQuantity: 1,
+    emailArr: [],
   },
   methods: {
-    
+    generateEmails() {
+      this.emailArr = [];
+      for (var i = 0; i < this.emailQuantity; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (obj) => {
+          const email = obj.data;
+          this.emailArr.push(email.response);
+        });
+      }
+      console.log(this.emailArr);
+    },
   }
 })
