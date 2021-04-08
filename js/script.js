a4bcfc4d7cc8e42955a5d163b6d0b5e9ab220243
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-04-08 11:33:20 +0200

    Fix

--- js/script.js
+++ js/script.js
@@ -3,15 +3,20 @@ const app = new Vue ({
   data: {
     emailQuantity: 1,
     emailArr: [],
+    btnDisable: '',
   },
   methods: {
     generateEmails() {
       this.emailArr = [];
+      this.btnDisable = 'disabled';
       for (var i = 0; i < this.emailQuantity; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then( (obj) => {
           const email = obj.data;
           this.emailArr.push(email.response);
+          if ( this.emailArr.length == this.emailQuantity ) {
+            this.btnDisable = '';
+          }
         });
       }
     },
